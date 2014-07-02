5581b7e93e4dfd34a5eac76c1df82d62ec740904
Alex Choi <heyalexchoi@gmail.com>
2014-07-02 17:49:04 -0400

    drafted subset

---
+++ Queues/src/Subset.java
@@ -0,0 +1,21 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+/**
+ * Created by alexchoi1 on 7/2/14.
+ */
+public class Subset {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        while (!StdIn.isEmpty()) {
+            String string = StdIn.readString();
+            queue.enqueue(string);
+        }
+
+        for (int i = 0; i < k; i ++) {
+            StdOut.println(queue.dequeue());
+        }
+
+    }
+}
