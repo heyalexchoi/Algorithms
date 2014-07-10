f318359f96ff280593d21bed02c2b703700f1cee
Alex Choi <heyalexchoi@gmail.com>
2014-07-09 20:45:42 -0400

    working on dedup sub segments

--- collinear/src/Fast.java
+++ collinear/src/Fast.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 /**
  * Created by alexchoi1 on 7/8/14.
@@ -10,37 +12,55 @@ public class Fast {
         new Fast(args);
     }
 
-    private void outputSegment(Point[] pointArray, int originIndex, int startIndex,  int segmentLength) {
-        //StdOut.println("print segment!");
-        //StdOut.println("origin" + pointArray[originIndex]);
+    private void outputSegment(LinkedList<Point> segment, HashSet<LinkedList<Point>> duplicates) {
 
-        Point[] outputArray = new Point[segmentLength];
-        outputArray[0] = pointArray[originIndex];
+        boolean isUnique = !duplicates.contains(segment);
 
-        int outputArrayCounter = 1;
 
-        for (int i = startIndex; i < startIndex + segmentLength - 1; i++) {
-          //  StdOut.println(pointArray[i] + " - >");
-          //  StdOut.println("slopeTo" + pointArray[originIndex].slopeTo(pointArray[i]));
-            outputArray[outputArrayCounter] = pointArray[i];
-            outputArrayCounter++;
+
+        if (!isUnique) {
+            StdOut.println("duplicate segment:");
+            for (Point point : segment) {
+                StdOut.println(point);
+            }
+            return;
         }
 
-        Arrays.sort(outputArray);
 
-        for (Point point : outputArray) {
-            StdOut.print(point + " -> ");
+
+        for (LinkedList<Point> duplicateSegment : duplicates) {
+                if (segment.containsAll(duplicateSegment)) {
+                    StdOut.println("duplicate subsegment:");
+                    for (Point point : duplicateSegment) {
+                        StdOut.println(point);
+                    }
+                    StdOut.println("containing segment:");
+                    for (Point point : duplicateSegment) {
+                        StdOut.println(point);
+                    }
+                    return;
+                }
         }
-        outputArray[0].drawTo(outputArray[segmentLength-1]);
 
-        //pointArray[originIndex].drawTo(pointArray[startIndex + segmentLength - 2]);
+        if (isUnique) {
+            StdOut.println("unique segment:");
+            for (Point point : segment) {
+                StdOut.println(point);
+            }
+            duplicates.add(segment);
+            return;
+        }
+
+
+
     }
         private Fast(String[] args) {
             String filename = args[0];
             In input = new In(filename);
 
             int N = input.readInt();
-            Point[] pointsArray = new Point[N];
+            Point[] naturalSortPoints = new Point[N];
+            HashSet<LinkedList<Point>> duplicates = new HashSet<LinkedList<Point>>();
 
             StdDraw.setXscale(0, 32768);
             StdDraw.setYscale(0, 32768);
@@ -52,75 +72,47 @@ public class Fast {
                 int y = input.readInt();
 
                 Point point = new Point(x, y);
-                Point point2 = new Point(x, y);
-
-                pointsArray[i] = point;
-
+                naturalSortPoints[i] = point;
                 point.draw();
             }
 
-            Arrays.sort(pointsArray);
+            Arrays.sort(naturalSortPoints);
 
-            // for each point p, find slope against each other point
-            // and sort those points to find segments
-            // actually dont need to iterate through last few... min segment length is 4
+            // loop through each naturally sorted point
             for (int i = 0; i < N - 1; i++) {
 
-                Point point = pointsArray[i];
-                //StdOut.println("SLOPE ORDER SORT FOR i" + point);
-                Arrays.sort(pointsArray, i + 1, N, point.SLOPE_ORDER);
-                for (int s = i; s < N; s ++) {
-                    //StdOut.println(pointsArray[s]);
-                }
+                Point point = naturalSortPoints[i];
 
-                // look for 3 or more adjacent points
-                // with same slope - those will be collinear with p
-                int segmentLength = 2;
+                Point[] slopeSortPoints = naturalSortPoints.clone();
+                Arrays.sort(slopeSortPoints, i + 1, N, point.SLOPE_ORDER);
+
+                LinkedList<Point> segment = new LinkedList<Point>();
+                segment.add(point);
+
                 // negative zero is not used anywhere else, so it is a safe init value
                 double previousSlopeTo = -0.0;
 
                 for (int j = i + 1; j < N; j++) {
-                    Point otherPoint = pointsArray[j];
-                   // StdOut.println("segmentLength: " + segmentLength + " slope to: " + point.slopeTo(otherPoint)
-                    //        + " point: " + point + " otherpoint: " + otherPoint);
 
-                    // matching slope: add to adjacent count
+                    Point otherPoint = slopeSortPoints[j]; // i in both arrays should be same
+
                     if (point.slopeTo(otherPoint) == previousSlopeTo) {
-                        segmentLength ++;
-                        //StdOut.println("Slope match found. Increment segment length to: " + segmentLength);
 
-                        // not end of loop. print segment if next point is not collinear.
-                        if (j != N - 1) {
-                            //StdOut.println("not end of loop. analyze next point");
-                            Point nextPoint = pointsArray[j+1];
-                            if (point.slopeTo(nextPoint) != previousSlopeTo && segmentLength > 3) {
-                                //StdOut.println("next point slop not match. segment length > 3. OUTPUT");
-                                outputSegment(pointsArray,i,j+2-segmentLength,segmentLength);
-                                segmentLength = 2;
-                            }
+                        Point previousPoint = slopeSortPoints[j - 1];
+                        if (segment.peekLast() != previousPoint) {
+                            segment.add(previousPoint);
                         }
-                        // end of loop. print if segment long enough
-                        else if (segmentLength > 3) {
-                            //StdOut.println("end of loop. segmentLength>3. OUTPUT");
-                            outputSegment(pointsArray,i,j+2-segmentLength,segmentLength);
-                        }
+                        segment.add(otherPoint);
                     }
-                    // not matching slope. go on
-                    else {
-                       // StdOut.println("no match. proceed");
-                        segmentLength = 2;
+
+                    // segment is sufficient size
+                    if (segment.size() > 3
+                            // and either is last iteration or next point's slope doesn't match (last point in segment)
+                            && (j == N-1 || point.slopeTo(slopeSortPoints[j+1]) != point.slopeTo(otherPoint))) {
+                        outputSegment(segment,duplicates);
                     }
                     previousSlopeTo = point.slopeTo(otherPoint);
                 }
-
             }
-
-
         }
-
-
-
-
-
-
 }
