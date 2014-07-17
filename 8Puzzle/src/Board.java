f9904af7bf71dd878d26dde6984eb0884e9c3114
Alex Choi <heyalexchoi@gmail.com>
2014-07-17 18:58:03 -0400

    switched representation two 2D char array. written much of Board including distances. not tested yet.

--- 8Puzzle/src/Board.java
+++ 8Puzzle/src/Board.java
@@ -2,93 +2,125 @@
  * Created by alexchoi1 on 7/17/14.
  */
 
-public class Board {
-
-    // array representation of Board
-    private block[] board;
-    // array representation of Goal
-    private final block[] goal;
-    // length/width of Board
-    private final int N;
-
-    // represents board block for both Board and Goal
-    private class block {
-        int x;
-        int y;
-        int value;
-    }
-
-    // returns 1-D array index for 2-D array index pair
+/*
+ // returns 1-D array index for 2-D array index pair
     // row i, column j
     private int indexForCoordinates(int i, int j) {
         return (i * N) + j;
     }
+ */
+public class Board {
+
+    // array representation of Board
+    private char[][] board;
+    // length/width of Board
+    private final char N;
+    // cached manhattan distance value. init -1, to mark that distance has not been calculated yet
+    private int manhattan = -1;
 
     // construct a board from an N-by-N array of blocks
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
-
-        N = blocks.length;
-
-        this.board = new block[N*N];
-        this.goal = new block[N*N];
-
+        N = (char)blocks.length;
+        this.board = new char[N][N];
         // for each row i
-        for (int i = 0; i < N; i ++) {
+        for (char i = 0; i < N; i++) {
             // for each column j
-            for (int j = 0; j < N; j++) {
-
-                int index = indexForCoordinates(i,j);
-
-                block boardBlock = new block();
-                boardBlock.x = j;
-                boardBlock.y = i;
-                boardBlock.value = blocks[i][j];
-                board[index] = boardBlock;
-
-                block goalBlock = new block();
-                goalBlock.x = j;
-                goalBlock.y = i;
-
-                if (index < N * N - 1) {
-                    goalBlock.value = index+1;
+            for (char j = 0; j < N; j++) {
+                board[i][j] = (char)blocks[i][j];
                 }
-                else {
-                    goalBlock.value = 0;
-                }
-
-                goal[index] = goalBlock;
             }
-        }
-  }
-
-
+    }
 
     // board dimension N
     public int dimension() {
         return N;
     }
 
+    // returns goal value for i,j coordinate pair
+    // goal value increments left to right, top to bottom; last value 0
+    private char goal(char i, char j) {
+        if (i == N - 1 && j == N - 1) {
+            return 0;
+        } else {
+            return (char)((i * N) + j + 1);
+        }
+    }
     // number of blocks out of place
     public int hamming() {
-        return 0;
-    }
-
-    // sum of Manhattan distances between blocks and goal
-    public int manhattan() {
+        // compare board to goal, and count how many blocks have different values
         int count = 0;
 
+        for (char i = 0; i < N; i++) {
+            for (char j = 0; j < N; j++) {
+                if (board[i][j] != goal(i,j)) {
+                    count++;
+                }
+            }
+        }
         return count;
+    }
+
+    // class used to store coordinate pairs to calculate manhattan distance
+    private class coordinatePair {
+        char i;
+        char j;
+    }
+    // function used to calculate manhattan distance
+    // returns inverse of goal(): the i,j coordinate pair for a goal value
+    private coordinatePair coordinatesForGoalValue(char value) {
+        coordinatePair pair = new coordinatePair();
+        if (value == 0) {
+            pair.i = (char)(N - 1);
+            pair.j = (char)(N - 1);
+        } else {
+            pair.i = (char)((value - 1) / N);
+            pair.j = (char)((value - 1) % N);
+        }
+        return pair;
+    }
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+        // compare each value on board to corresponding value on goal
+        // add up horizontal and vertical distance
+        // return cached value if available. manhattan inits to -1
+        if (manhattan != -1) {
+            return manhattan;
+        }
+
+        int distance = 0;
+
+        for (char i = 0; i < N; i ++) {
+           for (char j = 0; j < N; j++) {
+               // compare board coordinates to goal coordinates for each board/goal value
+               char boardValue = board[i][j];
+               coordinatePair goalCoordinates = coordinatesForGoalValue(boardValue);
+               distance += Math.abs(goalCoordinates.i - i) + Math.abs(goalCoordinates.j - j);
+           }
+        }
+        return distance;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-        return false;
+        return hamming() == 0;
     }
 
     // a board obtained by exchanging two adjacent blocks in the same row
     public Board twin() {
-        return new Board(new int[0][0]);
+
+        // copy our board
+        int[][] blocks = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 2; j < N; j++) {
+                blocks[i][j] = board[i][j];
+            }
+        }
+        // switch first two blocks
+        blocks[0][0] = board[0][1];
+        blocks[0][1] = board[0][0];
+
+        return new Board(blocks);
     }
 
     // does this board equal y?
